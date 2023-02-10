4d3f0050e36951468014cea05f3fa42e8d8bb886
dohoonkim <dohoonkim023@gmail.com>
2023-02-10 17:47:34 +0900

    [env] 환경구축 - spring, lombok, h2, jpa - ffmpeg

---
+++ src/main/java/com/example/videouploadapi/VideoUploadApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.videouploadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoUploadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoUploadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/videouploadapi/VideoUploadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videouploadapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoUploadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
